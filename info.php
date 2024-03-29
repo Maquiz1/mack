e6b6510a3820eba08a6e2d0a719ec78223b4bcf8
Maquiz1 <manquiz92@gmail.com>
2024-03-29 16:17:31 +0300

    fix registration

--- info.php
+++ info.php
@@ -314,7 +314,7 @@ if ($user->isLoggedIn()) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Lung Cancer Database | Info</title>
+    <title>Mack Database | Info</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
