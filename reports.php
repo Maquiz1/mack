338bbe2ded17120411c885f320a9e6a27a0c137b
Maquiz1 <manquiz92@gmail.com>
2024-08-10 22:29:49 +0300

    fix all

--- reports.php
+++ reports.php
@@ -8,9 +8,10 @@ $random = new Random();
 
 if ($user->isLoggedIn()) {
     try {
+        $registered = $override->getCount('clients', 'status', 1);
         $screened = $override->getCount('eligibility', 'status', 1);
         $enrolled = $override->getCount2('clients', 'status', 1, 'enrolled', 1);
-        $ListByMonthAllTables = $override->ListByMonthAllTables('clients', 'hiv_history_and_medication', 'eligibility', 'enrollments', 'risk_factors', 'medications', 'chronic_illnesses', 'laboratory_results', 'radiological_investigations', 'create_on');  
+        $ListByMonthAllTables = $override->ListByMonthAllTables('clients', 'hiv_history_and_medication', 'eligibility', 'enrollments', 'risk_factors', 'medications', 'chronic_illnesses', 'laboratory_results', 'radiological_investigations', 'create_on');
         $successMessage = 'Report Successful Created';
     } catch (Exception $e) {
         die($e->getMessage());
@@ -20,7 +21,7 @@ if ($user->isLoggedIn()) {
 }
 
 
-$span0 = 9;
+$span0 = 10;
 $span1 = 9;
 $span2 = 9;
 
@@ -31,7 +32,7 @@ $file_name = $title . '.pdf';
 
 $output = ' ';
 
-    $output .= '
+$output .= '
             <table width="100%" border="1" cellpadding="5" cellspacing="0">
                 <tr>
                     <td colspan="' . $span0 . '" align="center" style="font-size: 18px">
@@ -40,7 +41,7 @@ $output = ' ';
                 </tr>
                 <tr>
                     <td colspan="' . $span0 . '" align="center" style="font-size: 18px">
-                        <b>Total Screened ( ' . $screened . ' ):  Total Enrolled( ' . $enrolled . ' )</b>
+                        <b>Total Registered ( ' . $registered . ' ): Total Screened ( ' . $screened . ' ):  Total Enrolled( ' . $enrolled . ' )</b>
                     </td>
                 </tr>
                 <tr>
@@ -50,8 +51,9 @@ $output = ' ';
                             <tr>
                                 <th rowspan="1">No</th>
                                 <th rowspan="1">PERIOD</th>
+                                <th rowspan="1">Registered</th>
                                 <th rowspan="1">HIV History And Medication</th>
-                                <th rowspan="1">Eligibility </th>
+                                <th rowspan="1">Screening ( Eligibility ) </th>
                                 <th rowspan="1">Risk Factors</th>
                                 <th rowspan="1">Medications / Short-term illness</th>
                                 <th rowspan="1">Chronic Illnesses</th>
@@ -60,23 +62,26 @@ $output = ' ';
                             </tr>
             ';
 
-    // Load HTML content into dompdf
-    $x = 1;
-    foreach ($ListByMonthAllTables as $row) {
-        $crf2 = $override->getCount('hiv_history_and_medication', 'status', 1);        
-        $crf4 = $override->getCount('eligibility', 'status',1);
-        $crf5 = $override->getCount('risk_factors', 'status', 1);
-        $crf6 = $override->getCount('medications', 'status', 1);
-        $crf7 = $override->getCount('chronic_illnesses', 'status', 1);
-        $crf8 = $override->getCount('laboratory_results', 'status', 1);
-        $crf9 = $override->getCount('radiological_investigations', 'status', 1);
+// Load HTML content into dompdf
+$x = 1;
+foreach ($ListByMonthAllTables as $row) {
+    $crf1 = $override->getCount('clients', 'status', 1);
+    $crf2 = $override->getCount('hiv_history_and_medication', 'status', 1);
+    $crf3 = $override->getCount('eligibility', 'status', 1);
+    $crf4 = $override->getCount('enrollments', 'status', 1);
+    $crf5 = $override->getCount('risk_factors', 'status', 1);
+    $crf6 = $override->getCount('medications', 'status', 1);
+    $crf7 = $override->getCount('chronic_illnesses', 'status', 1);
+    $crf8 = $override->getCount('laboratory_results', 'status', 1);
+    $crf9 = $override->getCount('radiological_investigations', 'status', 1);
 
-        $output .= '
+    $output .= '
                 <tr>
                     <td>' . $x . '</td>
                     <td>' . $row['month']  . '</td>
+                    <td>' . $row['count1']  . '</td>
                     <td>' . $row['count2']  . '</td>
-                    <td>' . $row['count4']  . '</td>
+                    <td>' . $row['count3']  . '</td>
                     <td>' . $row['count5']  . '</td>
                     <td>' . $row['count6']  . '</td>
                     <td>' . $row['count7']  . '</td>
@@ -85,14 +90,15 @@ $output = ' ';
                 </tr>
             ';
 
-        $x += 1;
-    }
+    $x += 1;
+}
 
-    $output .= '
+$output .= '
                 <tr>
                     <td align="right" colspan="2"><b>Total</b></td>
+                    <td align="center"><b>' . $crf1 . '</b></td>
                     <td align="center"><b>' . $crf2 . '</b></td>
-                    <td align="center"><b>' . $crf4 . '</b></td>
+                    <td align="center"><b>' . $crf3 . '</b></td>
                     <td align="center"><b>' . $crf5 . '</b></td>
                     <td align="center"><b>' . $crf6 . '</b></td>
                     <td align="center"><b>' . $crf7 . '</b></td>
