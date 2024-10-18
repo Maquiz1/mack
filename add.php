e054b7e4cf907329e468219040f857ef8fb2bc84
Maquiz1 <manquiz92@gmail.com>
2024-10-18 13:28:12 +0300

    fix all

--- add.php
+++ add.php
@@ -7771,7 +7771,7 @@ if ($user->isLoggedIn()) {
     <script src="myjs/hiv_history/second_line.js"></script>
     <script src="myjs/hiv_history/third_line.js"></script>
     <script src="myjs/hiv_history/same_regimen.js"></script>
-    <script src="myjs/hiv_history/unwell1.js"></script>
+    <script src="myjs/hiv_history/unwell.js"></script>
 
 
 
