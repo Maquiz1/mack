d95cb4fe7d8ce52f03bc10c4868ec30781d79ec4
Maquiz1 <manquiz92@gmail.com>
2024-10-25 14:05:28 +0300

    fix all

--- myjs/radiology/chamber_dimensions/left_atrium.js
+++ myjs/radiology/chamber_dimensions/left_atrium.js
@@ -1,19 +1,19 @@
 const left_atrium1 = document.getElementById("left_atrium1");
 const left_atrium2 = document.getElementById("left_atrium2");
 
-const lf_atriu_parasternal = document.getElementById("lf_atriu_parasternal");
-const lf_atrium_4chamb_long = document.getElementById("lf_atrium_4chamb_long");
-const lf_atrium_4chamb_minor = document.getElementById("lf_atrium_4chamb_minor");
+const lf_atriu_parasternal3 = document.getElementById("lf_atriu_parasternal3");
+const lf_atrium_4chamb_long3 = document.getElementById("lf_atrium_4chamb_long3");
+const lf_atrium_4chamb_minor3 = document.getElementById("lf_atrium_4chamb_minor3");
 
 function toggleElementVisibility() {
   if (left_atrium1.checked) {
-    lf_atriu_parasternal.style.display = "block";
-    lf_atrium_4chamb_long.style.display = "block";
-    lf_atrium_4chamb_minor.style.display = "block";
+    lf_atriu_parasternal3.style.display = "block";
+    lf_atrium_4chamb_long3.style.display = "block";
+    lf_atrium_4chamb_minor3.style.display = "block";
   } else {
-    lf_atriu_parasternal.style.display = "none";
-    lf_atrium_4chamb_long.style.display = "none";
-    lf_atrium_4chamb_minor.style.display = "none";
+    lf_atriu_parasternal3.style.display = "none";
+    lf_atrium_4chamb_long3.style.display = "none";
+    lf_atrium_4chamb_minor3.style.display = "none";
   }
 }
 
